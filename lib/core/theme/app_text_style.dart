c5583b7405978d0ca65f7b02ee6cb88ed7084205
Makaruos Emad <makaruosemad235@gmail.com>
2026-05-11 01:55:36 +0300

    add text styles

---
+++ lib/core/theme/app_text_style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle bold24Black = TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle regular16Grey = TextStyle(
+    color: Colors.grey,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular16Blue = TextStyle(
+    color: Colors.blue,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
